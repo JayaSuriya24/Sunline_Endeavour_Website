a476a0e22d16358920d02ec90de3e10cddfdaf93
Jaya Suriya Manickavasagam <jayasuriya2410@gmail.com>
2024-01-09 10:49:46 +0530

    About Us Section is completed

--- Script.js
+++ Script.js
@@ -7,3 +7,20 @@ function showMenu() {
 function hideMenu() {
     navLinks.style.right = "-200px";
 }
+
+// script code for toggle text
+const parentContainer = document.querySelector('.about-us');
+
+parentContainer.addEventListener('click', event => {
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('red-btn');
+    if (!isReadMoreBtn) return;
+
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ?
+        "Read Less" : "Read More";
+})
+
